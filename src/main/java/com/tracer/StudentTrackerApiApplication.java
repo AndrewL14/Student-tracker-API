ed8a818f2e7d12c9554a2cec772bc7d5042458d1
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-11-27 07:06:20 -0600

    initial setup

---
+++ src/main/java/com/tracer/StudentTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrackerApiApplication.class, args);
+	}
+
+}
