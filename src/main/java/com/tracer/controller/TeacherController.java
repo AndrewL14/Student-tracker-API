915c05cd0a61d1c275e07d281dde96cb8117b808
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-11-30 06:09:06 -0600

    Implmented service class
    Implmented the TeacherService class and redid paramters
    and mapping paths for each http method.

--- src/main/java/com/tracer/controller/TeacherController.java
+++ src/main/java/com/tracer/controller/TeacherController.java
@@ -1,6 +1,11 @@
 package com.tracer.controller;
 
 import com.tracer.model.Student;
+import com.tracer.model.request.AddStudentRequest;
+import com.tracer.model.request.EditStudentRequest;
+import com.tracer.model.request.GetStudentRequest;
+import com.tracer.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,29 +16,35 @@ import java.util.List;
 @RequestMapping("/teacher")
 @CrossOrigin("*")
 public class TeacherController {
-    @GetMapping("all")
-    public ResponseEntity<List<Student>> getAllStudents() {
-        return null;
-//        return new ResponseEntity<List<Student>>(null);
+    @Autowired
+    private TeacherService service;
+
+    @GetMapping("/all{username}")
+    public ResponseEntity<List<Student>> getAllStudents(@RequestParam String username) {
+        return new ResponseEntity<List<Student>>(
+                service.getAllStudentsByTeacherUsername(username),
+                HttpStatus.OK
+        );
     }
-    @GetMapping("unique")
-    public ResponseEntity<List<Student>> getStudentsByName() {
-        return null;
-//        return new ResponseEntity<List<Student>>(null);
+    @GetMapping("/unique")
+    public ResponseEntity<List<Student>> getStudentsByName(@RequestBody GetStudentRequest request) {
+        return new ResponseEntity<List<Student>>(
+                service.getStudentsByName(request.getTeacherUsername() , request.getStudentName()),
+                HttpStatus.OK
+        );
     }
-    @PutMapping("add")
-    public ResponseEntity<Student> addStudent() {
-        return null;
-//        return new ResponseEntity<Student>(null);
+    @PutMapping("/add")
+    public ResponseEntity<List<Student>> addStudent(@RequestBody AddStudentRequest request) {
+        return new ResponseEntity<List<Student>>(service.addStudent(request),
+                HttpStatus.OK);
     }
-    @PostMapping("edit")
-    public ResponseEntity<Student> editExistingStudent() {
-        return null;
-//        return new ResponseEntity<Student>(null);
+    @PostMapping("/edit")
+    public ResponseEntity<List<Student>> editExistingStudent(@RequestBody EditStudentRequest request) {
+        return new ResponseEntity<List<Student>>(service.editExistingStudent(request),
+                HttpStatus.OK);
     }
-    @GetMapping("delete")
-    public ResponseEntity<Boolean> deleteExistingStudent() {
-        return null;
-//        return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+    @GetMapping("/delete")
+    public ResponseEntity<?> deleteExistingStudent(@RequestBody GetStudentRequest request) {
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
