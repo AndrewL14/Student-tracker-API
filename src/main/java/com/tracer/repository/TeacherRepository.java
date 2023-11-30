babbb2f17b375026761458fd2732d5c901966bc0
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-11-30 06:13:15 -0600

    Created repository
    Created repo interfaces for both student and teacher entities
    To allow the program to interact with the database.

--- src/main/java/com/tracer/repository/TeacherRepository.java
+++ src/main/java/com/tracer/repository/TeacherRepository.java
@@ -2,6 +2,10 @@ package com.tracer.repository;
 
 import com.tracer.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findByUsername(String username);
 }
