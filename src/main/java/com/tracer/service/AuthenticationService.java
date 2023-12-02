b3838a91cbb6d1b6e5557441b74698e2cdb62b3b
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-02 09:30:16 -0600

    Created Auth service
    the service well talk directly to the database and handle all logic for
    registering and logining in users.

--- src/main/java/com/tracer/service/AuthenticationService.java
+++ src/main/java/com/tracer/service/AuthenticationService.java
@@ -1,2 +1,63 @@
-package com.tracer.service;public class AuthenticationService {
+package com.tracer.service;
+
+import com.tracer.model.Role;
+import com.tracer.model.Teacher;
+import com.tracer.model.response.LoginResponse;
+import com.tracer.repository.AuthorityRepository;
+import com.tracer.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class AuthenticationService {
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private AuthorityRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public Teacher registerUser(String username, String password){
+
+        String encodedPassword = passwordEncoder.encode(password);
+        Role userRole = roleRepository.findByAuthority("TEACHER").get();
+
+        Set<Role> authorities = new HashSet<>();
+
+        authorities.add(userRole);
+
+        return teacherRepository.save(new Teacher(username, encodedPassword, new ArrayList<>(),
+                authorities));
+    }
+
+    public LoginResponse loginUser(String username, String password){
+
+        Authentication authentication = authenticationManager.authenticate(
+                   new UsernamePasswordAuthenticationToken(username, password)
+        );
+        String token = tokenService.generateJwt(authentication);
+
+        return new LoginResponse(teacherRepository.findByUsername(username)
+                .orElseThrow(NullPointerException::new),
+                token);
+
+    }
 }
