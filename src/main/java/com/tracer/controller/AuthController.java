9d5b2532c2acc1cdcc3194cf455a7ae84e3eb562
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-02 09:25:08 -0600

    Created Auth controller
    Allows for login and registration of teachers

--- src/main/java/com/tracer/controller/AuthController.java
+++ src/main/java/com/tracer/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.tracer.controller;
 
+import com.tracer.model.Teacher;
 import com.tracer.model.request.LoginRequest;
+import com.tracer.model.request.RegistrationRequest;
 import com.tracer.model.response.LoginResponse;
 import com.tracer.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
-public class AuthenticationController {
+public class AuthController {
     @Autowired
     private AuthenticationService authService;
+
+    @PostMapping("/register")
+    public Teacher registerUser(@RequestBody RegistrationRequest request) {
+        return authService.registerUser(request.getUsername(), request.getPassword());
+    }
 
     @PostMapping("/login")
     public LoginResponse loginUser(@RequestBody LoginRequest request) {
