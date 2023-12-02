081e7d35daf40b2dbd25954041d39c644e6e0771
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-02 09:27:40 -0600

    Created login / registration DTO
    Created a request and response class for login, and a request class
    for regsitration.

--- src/main/java/com/tracer/model/response/LoginResponse.java
+++ src/main/java/com/tracer/model/response/LoginResponse.java
@@ -1,30 +1,16 @@
-package com.test.practice.entity;
+package com.tracer.model.response;
 
-public class LoginResponse{
-    private ApplicationUser user;
+import com.tracer.model.Teacher;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private Teacher teacher;
     private String jwt;
-
-    public LoginResponse() {
-    }
-
-    public LoginResponse(ApplicationUser user , String jwt) {
-        this.user = user;
-        this.jwt = jwt;
-    }
-
-    public ApplicationUser getUser() {
-        return user;
-    }
-
-    public void setUser(ApplicationUser user) {
-        this.user = user;
-    }
-
-    public String getJwt() {
-        return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
-    }
 }
