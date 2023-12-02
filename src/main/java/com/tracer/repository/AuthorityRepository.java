5ea703cc45a6037d97f054a3cd084735738581aa
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-02 09:29:06 -0600

    created Auth repo

--- src/main/java/com/tracer/repository/AuthorityRepository.java
+++ src/main/java/com/tracer/repository/AuthorityRepository.java
@@ -1,2 +1,10 @@
-package com.tracer.repository;public interface AuthorityRepository {
+package com.tracer.repository;
+
+import com.tracer.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthorityRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByAuthority(String authority);
 }
