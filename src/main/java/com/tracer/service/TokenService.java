5c47f3b11a94f1f0a304b07ba4214a0f755ed157
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-11 13:13:56 -0600

    Removed unnecessary imports

--- src/main/java/com/tracer/service/TokenService.java
+++ src/main/java/com/tracer/service/TokenService.java
@@ -3,7 +3,6 @@ package com.tracer.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
@@ -11,9 +10,6 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
