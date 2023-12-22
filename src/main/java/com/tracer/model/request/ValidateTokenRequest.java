5cdc7b06ef4b299f887034c0fda630ea41a3c0bf
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-22 02:34:25 -0600

    created token request DTO

--- src/main/java/com/tracer/model/request/ValidateTokenRequest.java
+++ src/main/java/com/tracer/model/request/ValidateTokenRequest.java
@@ -1,2 +1,15 @@
-package com.tracer.model.request;public class ValidateTokenRequest {
+package com.tracer.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ValidateTokenRequest {
+    String username;
+    String jwt;
 }
