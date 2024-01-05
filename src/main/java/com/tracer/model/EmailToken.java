c2357ae62b7ce34a6d9ae38c69a00650bce9f0b2
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-01-05 13:22:32 -0600

    created email token model

--- src/main/java/com/tracer/model/EmailToken.java
+++ src/main/java/com/tracer/model/EmailToken.java
@@ -1,2 +1,40 @@
-package com.tracer.model;public class EmailToken {
+package com.tracer.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "email_tokens")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class EmailToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "email_token_id")
+    private Long id;
+    private String token;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Teacher teacher;
+    private LocalDateTime createdAt;
+    private LocalDateTime verifiedAt;
+    private LocalDateTime expiresAt;
+    private boolean isExpired;
+    private boolean isVerified;
+
+    public EmailToken(String token, Teacher teacher, LocalDateTime createdAt, LocalDateTime expiresAt) {
+       this.token = token;
+       this.teacher = teacher;
+       this.createdAt = createdAt;
+       this.verifiedAt = null;
+       this.expiresAt = expiresAt;
+       this.isExpired = false;
+       this.isVerified = false;
+    }
 }
