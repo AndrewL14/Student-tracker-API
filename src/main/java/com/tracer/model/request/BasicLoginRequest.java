5c213c1a7a668950a1858da1844c338e0b979958
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-01-05 13:23:48 -0600

    renamed to basic login request

--- src/main/java/com/tracer/model/request/BasicLoginRequest.java
+++ src/main/java/com/tracer/model/request/BasicLoginRequest.java
@@ -1,30 +1,13 @@
 package com.tracer.model.request;
 
-public class LoginRequest {
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class BasicLoginRequest {
     private String username;
     private String password;
-
-    public LoginRequest() {
-    }
-
-    public LoginRequest(String username , String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
