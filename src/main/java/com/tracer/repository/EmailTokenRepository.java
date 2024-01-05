d42e6dc61150119aa1d3c0f425750ea329142ced
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-01-05 13:28:41 -0600

    created a emailToken repo

--- src/main/java/com/tracer/repository/EmailTokenRepository.java
+++ src/main/java/com/tracer/repository/EmailTokenRepository.java
@@ -1,2 +1,10 @@
-package com.tracer.repository;public interface EmailTokenRepository {
+package com.tracer.repository;
+
+import com.tracer.model.EmailToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailTokenRepository extends JpaRepository<EmailToken, Long> {
+    Optional<EmailToken> findByToken(String token);
 }
