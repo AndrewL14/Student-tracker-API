73d286546701a5a556f462668b3a777a67b071f9
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-03-12 00:01:52 -0500

    packaging miss-placement fix

--- src/main/java/com/tracer/controller/AuthController.java
+++ src/main/java/com/tracer/controller/AuthController.java
@@ -1,12 +1,11 @@
 package com.tracer.controller;
 
-import com.tracer.model.request.BasicLoginRequest;
-import com.tracer.model.request.CompletePasswordResetRequest;
-import com.tracer.model.request.EmailLoginRequest;
-import com.tracer.model.request.RegistrationRequest;
+import com.tracer.model.request.authentication.BasicLoginRequest;
+import com.tracer.model.request.authentication.CompletePasswordResetRequest;
+import com.tracer.model.request.authentication.EmailLoginRequest;
+import com.tracer.model.request.authentication.RegistrationRequest;
 import com.tracer.model.response.LoginResponse;
 import com.tracer.service.AuthenticationService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
