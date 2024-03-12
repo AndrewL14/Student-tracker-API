c4817981eb36b27aa3153755c764d3249da656ce
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-03-11 23:55:32 -0500

    Created authority labels for users

--- src/main/java/com/tracer/constant/Authority.java
+++ src/main/java/com/tracer/constant/Authority.java
@@ -1,2 +1,18 @@
-package com.tracer.constant;public enum Authority {
+package com.tracer.constant;
+
+public enum Authority {
+    TEACHER("Teacher"),
+    STUDENT("Student"),
+    ADMIN("Admin");
+
+    private final String label;
+
+    private Authority(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
 }
