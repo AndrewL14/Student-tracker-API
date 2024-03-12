1935264c15554664079f0f1c28bf37d88b2a64cb
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-03-12 00:01:01 -0500

    updated Run command for assignment

--- src/main/java/com/tracer/StudentTrackerApiApplication.java
+++ src/main/java/com/tracer/StudentTrackerApiApplication.java
@@ -3,6 +3,8 @@ package com.tracer;
 import com.tracer.model.Role;
 import com.tracer.model.Student;
 import com.tracer.model.Teacher;
+import com.tracer.model.assignments.Assignment;
+import com.tracer.model.assignments.AssignmentType;
 import com.tracer.repository.AuthorityRepository;
 import com.tracer.repository.StudentRepository;
 import com.tracer.repository.TeacherRepository;
@@ -10,13 +12,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
+@EnableScheduling
 public class StudentTrackerApiApplication {
 
 	public static void main(String[] args) {
@@ -34,9 +41,12 @@ public class StudentTrackerApiApplication {
 			Set<Role> authorities = new HashSet<>();
 			authorities.add(role);
 
+			Assignment assignment = new Assignment("test", 100.0,
+					false, true, LocalDate.now(), AssignmentType.ASSIGNMENT.toString());
 			Student student = new Student();
 			student.setName("Jhon Smith");
 			student.setGrade(BigDecimal.valueOf(1));
+			student.setAssignments(List.of(assignment));
 			student.setPeriod(1);
 
 			Set<Student> temp = new HashSet<>();
