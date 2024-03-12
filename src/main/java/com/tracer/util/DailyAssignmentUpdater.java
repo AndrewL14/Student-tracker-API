064f4e8fb508ce1915e74ae1a61aea9644456d23
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-03-11 23:59:52 -0500

    made a way to update assignments automatically.

--- src/main/java/com/tracer/util/DailyAssignmentUpdater.java
+++ src/main/java/com/tracer/util/DailyAssignmentUpdater.java
@@ -1,2 +1,37 @@
-package com.tracer.util;public class DailyAssignmentUpdater {
+package com.tracer.util;
+
+import com.tracer.model.assignments.Assignment;
+import com.tracer.repository.AssignmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Service
+public class DailyAssignmentUpdater {
+    @Autowired
+    private AssignmentRepository assignmentRepository;
+
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void CheckIfAssignmentIsLate() {
+        List<Assignment> assignments = assignmentRepository.findAll();
+        ExecutorService executorService = Executors.newFixedThreadPool(assignments.size() / 4);
+
+        for (Assignment assignment : assignments) {
+            executorService.submit(() -> {
+                if (isLate(assignment)) {
+                    assignment.setOverdue(true);
+                }
+            });
+        }
+    }
+
+    private boolean isLate(Assignment assignment) {
+        return assignment.getDueDate().isBefore(LocalDate.now()) && !assignment.isCompleted();
+    }
 }
