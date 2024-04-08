64eec2a04fcaf6b530a57bed93348057203c4248
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-08 00:54:45 -0500

    Created Token repo

--- src/main/java/com/tracer/repository/tokens/TokenRepository.java
+++ src/main/java/com/tracer/repository/tokens/TokenRepository.java
@@ -1,2 +1,15 @@
-package com.tracer.repository.tokens;public class TokenRepository {
+package com.tracer.repository.tokens;
+
+import com.tracer.model.tokens.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByUsername(String username);
+    List<Token> findAllByUsername(String username);
+    Optional<Token> findByUsernameAndJwt(String username, String jwt);
 }
