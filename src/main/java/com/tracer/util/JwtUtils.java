671b096c489f6efc73febe18cee5212cf0e10df2
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-08 00:58:58 -0500

    extract jwt / experiation functions

--- src/main/java/com/tracer/util/JwtUtils.java
+++ src/main/java/com/tracer/util/JwtUtils.java
@@ -1,2 +1,26 @@
-package com.tracer.util;public class JwtUtils {
+package com.tracer.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public class JwtUtils {
+    public static String extractJwtFromRequest(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
+    }
+
+    public static Long getTokenExpiration(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof String) {
+            String token = (String) principal;
+            Claims claims = Jwts.parser().parseClaimsJws(token).getBody();
+            return claims.getExpiration().getTime();
+        }
+        return null;
+    }
 }
