d7c2ce97f44caf5ac4cc6fb408d1cce3b79e2148
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-08 00:55:11 -0500

    added custom exeception for token

--- src/main/java/com/tracer/exception/ExpiredTokenException.java
+++ src/main/java/com/tracer/exception/ExpiredTokenException.java
@@ -1,2 +1,15 @@
-package com.tracer.exception;public class ExpiredTokenException {
+package com.tracer.exception;
+
+import java.io.Serial;
+
+public class ExpiredTokenException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 8210052284450171757L;
+
+    public ExpiredTokenException(String explanation) {
+        super(explanation);
+    }
+
+    public ExpiredTokenException() {
+    }
 }
