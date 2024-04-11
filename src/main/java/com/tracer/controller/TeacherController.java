74bc87bef900d6b7232e5f8df76737a87721b319
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-11 06:25:55 -0500

    implemented custom token validation

--- src/main/java/com/tracer/controller/TeacherController.java
+++ src/main/java/com/tracer/controller/TeacherController.java
@@ -25,7 +25,7 @@ public class TeacherController {
 
     @GetMapping("/all")
     public ResponseEntity<List<Student>> getAllStudents(HttpServletRequest servletRequest, Authentication authentication) {
-        tokenService.validateToken(servletRequest, authentication);
+        tokenService.validateJwt(servletRequest, authentication);
         return new ResponseEntity<List<Student>>(
                 service.getAllStudentsByTeacherUsername(authentication.getName()),
                 HttpStatus.OK
@@ -34,7 +34,7 @@ public class TeacherController {
     @GetMapping("/unique{studentName}")
     public ResponseEntity<List<Student>> getStudentsByName(@RequestParam String studentName,
                                                            HttpServletRequest servletRequest, Authentication authentication) {
-        tokenService.validateToken(servletRequest, authentication);
+        tokenService.validateJwt(servletRequest, authentication);
         return new ResponseEntity<List<Student>>(
                 service.getStudentsByName(authentication.getName() , studentName),
                 HttpStatus.OK
@@ -43,21 +43,21 @@ public class TeacherController {
     @PutMapping("/add")
     public ResponseEntity<List<Student>> addStudent(@RequestBody AddStudentRequest request,
                                                     HttpServletRequest servletRequest, Authentication authentication) {
-        tokenService.validateToken(servletRequest, authentication);
+        tokenService.validateJwt(servletRequest, authentication);
         return new ResponseEntity<List<Student>>(service.addStudent(request, authentication.getName()),
                 HttpStatus.OK);
     }
     @PostMapping("/edit")
     public ResponseEntity<List<Student>> editExistingStudent(@RequestBody EditStudentRequest request,
                                                              HttpServletRequest servletRequest, Authentication authentication) {
-        tokenService.validateToken(servletRequest, authentication);
+        tokenService.validateJwt(servletRequest, authentication);
         return new ResponseEntity<List<Student>>(service.editExistingStudent(request, authentication.getName()),
                 HttpStatus.OK);
     }
     @DeleteMapping("/delete{studentId}")
     public ResponseEntity<?> deleteExistingStudent(@RequestParam Long studentId,
                                                    HttpServletRequest servletRequest, Authentication authentication) {
-        tokenService.validateToken(servletRequest, authentication);
+        tokenService.validateJwt(servletRequest, authentication);
             service.deleteStudent(studentId, authentication.getName());
         return new ResponseEntity<>("", HttpStatus.OK);
     }
