9e5f46372f3d5e570ce3a9699f63da5b4f7265d8
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-11 06:17:10 -0500

    made refresh token repo

--- src/main/java/com/tracer/repository/tokens/RefreshTokenRepository.java
+++ src/main/java/com/tracer/repository/tokens/RefreshTokenRepository.java
@@ -1,2 +1,15 @@
-package com.tracer.repository.tokens;public interface RefreshTokenRepository {
+package com.tracer.repository.tokens;
+
+import com.tracer.model.tokens.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUsernameAndToken(String username, String token);
+    Optional<RefreshToken> findByToken(String token);
+    List<RefreshToken> findAllByUsername(String username);
 }
