f23f88c9e2ded3dcd6fda63d1b37cce38488c24c
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-05-29 16:20:09 -0500

    Created Assignments repo

--- src/main/java/com/tracer/repository/assignments/AssignmentsRepository.java
+++ src/main/java/com/tracer/repository/assignments/AssignmentsRepository.java
@@ -1,2 +1,9 @@
-package com.tracer.repository.assignments;public class AssignmentsRepository {
+package com.tracer.repository.assignments;
+
+import com.tracer.model.assignments.Assignments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssignmentsRepository extends JpaRepository<Assignments, Long> {
 }
