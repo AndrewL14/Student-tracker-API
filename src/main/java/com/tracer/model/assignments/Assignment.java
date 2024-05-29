84d8dee6213492f885b481e84398adf631dabe06
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-05-29 16:02:57 -0500

    Implemented Assignments entity.
    Made Assignment objects to refrence Assignments instead of student
    entiy.
    Made Json specifier for dueDate value.

--- src/main/java/com/tracer/model/assignments/Assignment.java
+++ src/main/java/com/tracer/model/assignments/Assignment.java
@@ -1,12 +1,14 @@
 package com.tracer.model.assignments;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "assignments")
+@Table(name = "assignment_table")
 @Data
 @EqualsAndHashCode
 @ToString
@@ -22,16 +24,22 @@ public class Assignment {
     private Double grade;
     private boolean completed;
     private boolean overdue;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dueDate;
     private String assignmentType;
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "assignments_id")
+    private Assignments assignmentList;
 
     public Assignment(String name , double grade , boolean completed ,
-                      boolean overdue , LocalDate dueDate, String assignmentType) {
+                      boolean overdue , LocalDate dueDate, String assignmentType, Assignments assignmentList) {
         this.name = name;
         this.grade = grade;
         this.completed = completed;
         this.overdue = overdue;
         this.dueDate = dueDate;
         this.assignmentType = assignmentType;
+        this.assignmentList = assignmentList;
     }
 }
