89f84a9d0ff9e66d594ca88934a643f3a4ffb25f
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-05-29 16:08:54 -0500

    added custom delete request

--- src/main/java/com/tracer/model/request/student/DeleteStudentRequest.java
+++ src/main/java/com/tracer/model/request/student/DeleteStudentRequest.java
@@ -1,2 +1,7 @@
-package com.tracer.model.request.student;public class DeleteStudentRequest {
+package com.tracer.model.request.student;
+
+public record DeleteStudentRequest(
+        int period,
+        Long studentId
+) {
 }
