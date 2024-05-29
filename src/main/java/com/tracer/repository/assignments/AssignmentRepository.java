fb09ae303aff3c5f142efaa4762f3c566abc8113
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-05-29 16:19:54 -0500

    added repo annoation

--- src/main/java/com/tracer/repository/assignments/AssignmentRepository.java
+++ src/main/java/com/tracer/repository/assignments/AssignmentRepository.java
@@ -1,7 +1,9 @@
-package com.tracer.repository;
+package com.tracer.repository.assignments;
 
 import com.tracer.model.assignments.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 }
