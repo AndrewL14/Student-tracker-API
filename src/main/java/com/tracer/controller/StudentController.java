d4e9c517ef5442a664977a0e4cfde9bf9578362d
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-05-30 15:19:20 -0500

    udpated response objects

--- src/main/java/com/tracer/controller/StudentController.java
+++ src/main/java/com/tracer/controller/StudentController.java
@@ -1,7 +1,8 @@
 package com.tracer.controller;
 
-import com.tracer.model.Student;
+import com.tracer.model.DTO.PrivateStudentDTO;
 import com.tracer.model.assignments.Assignment;
+import com.tracer.model.assignments.Assignments;
 import com.tracer.model.request.assignment.AddAssignmentRequest;
 import com.tracer.model.request.assignment.DeleteAssignmentRequest;
 import com.tracer.model.request.assignment.EditAssignmentRequest;
@@ -27,23 +28,23 @@ public class StudentController {
     private TokenService tokenService;
 
     @GetMapping("/get-student{id}")
-    public ResponseEntity<Student> getStudentById(@RequestParam Long id) {
-        return new ResponseEntity<Student>(service.getStudentById(id), HttpStatus.OK);
+    public ResponseEntity<PrivateStudentDTO> getStudentById(@RequestParam Long id) {
+        return new ResponseEntity<PrivateStudentDTO>(service.getStudentById(id), HttpStatus.OK);
     }
     @GetMapping("/get-all{id}")
-    public ResponseEntity<List<Assignment>> getAllAssignmentsByStudentId(@RequestParam Long id,
-                                                                         HttpServletRequest servletRequest, Authentication authentication ) {
+    public ResponseEntity<List<Assignments>> getAllAssignmentsByStudentId(@RequestParam Long id,
+                                                                          HttpServletRequest servletRequest, Authentication authentication ) {
         tokenService.validateJwt(servletRequest, authentication);
-        return new ResponseEntity<List<Assignment>>(
+        return new ResponseEntity<List<Assignments>>(
                 service.getAllAssignmentsByStudentId(id), HttpStatus.OK
         );
     }
 
     @PutMapping("/add")
-    public ResponseEntity<List<Assignment>> addNewAssignment(@RequestBody AddAssignmentRequest request,
-                                                             HttpServletRequest servletRequest, Authentication authentication ) {
+    public ResponseEntity<Assignments> addNewAssignment(@RequestBody AddAssignmentRequest request,
+                                                        HttpServletRequest servletRequest, Authentication authentication ) {
         tokenService.validateJwt(servletRequest, authentication);
-        return new ResponseEntity<List<Assignment>>(
+        return new ResponseEntity<Assignments>(
                 service.addNewAssignment(request), HttpStatus.CREATED
         );
     }
@@ -52,8 +53,8 @@ public class StudentController {
     public ResponseEntity<List<Assignment>> updateStatus(@RequestBody UpdateAssignmentStatusRequest request,
                                                          HttpServletRequest servletRequest, Authentication authentication ) {
         tokenService.validateJwt(servletRequest, authentication);
-        return new ResponseEntity<>(
-                service.updateCompleteStatus(request.getStudentId(), request.getAssignmentId()), HttpStatus.OK
+        return new ResponseEntity<List<Assignment>>(
+                service.updateCompleteStatus(request, authentication.getName()), HttpStatus.OK
         );
     }
 
