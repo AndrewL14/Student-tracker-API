720ecb5a6956351cf1e3f88fb16fb3156e1fe342
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-05-30 15:20:02 -0500

    Added new entity creation
    Added the new Assignments object and modified the order of creation.

--- src/main/java/com/tracer/StudentTrackerApiApplication.java
+++ src/main/java/com/tracer/StudentTrackerApiApplication.java
@@ -4,10 +4,13 @@ import com.tracer.model.Role;
 import com.tracer.model.Student;
 import com.tracer.model.Teacher;
 import com.tracer.model.assignments.Assignment;
+import com.tracer.model.assignments.Assignments;
 import com.tracer.model.assignments.AssignmentType;
+import com.tracer.repository.assignments.AssignmentRepository;
 import com.tracer.repository.AuthorityRepository;
 import com.tracer.repository.StudentRepository;
 import com.tracer.repository.TeacherRepository;
+import com.tracer.repository.assignments.AssignmentsRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,12 +18,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @SpringBootApplication
 @EnableScheduling
@@ -33,28 +32,57 @@ public class StudentTrackerApiApplication {
 	// Use this method only when running in dev mode.
 	@Bean
 	CommandLineRunner run(TeacherRepository teacherRepository, StudentRepository studentRepository,
-						  PasswordEncoder passwordEncoder, AuthorityRepository authorityRepository) {
+						  PasswordEncoder passwordEncoder, AuthorityRepository authorityRepository,
+						  AssignmentRepository assignmentRepository, AssignmentsRepository assignmentsRepository) {
 		return args -> {
 			if (teacherRepository.count() > 0) return;
+
+			// Create and save roles
 			Role role = authorityRepository.save(new Role("TEACHER"));
-			authorityRepository.save(new Role("test"));
+			Role studentRole = authorityRepository.save(new Role("STUDENT"));
 			Set<Role> authorities = new HashSet<>();
 			authorities.add(role);
+			Set<Role> studentAuthorities = new HashSet<>();
+			studentAuthorities.add(studentRole);
 
-			Assignment assignment = new Assignment("Adding and subtracting", 100.0,
-					true, false, LocalDate.now(), AssignmentType.ASSIGNMENT.toString());
-			Student student = new Student();
-			student.setName("Jhon Smith");
-			student.setGrade(BigDecimal.valueOf(100));
-			student.setAssignments(List.of(assignment));
-			student.setPeriod(1);
+			// Create Subjects
+			Set<String> subjects = new HashSet<>();
+			subjects.add("math");
 
-			List<Student> temp = new ArrayList<>();
-			temp.add(student);
-			Teacher teacher = new Teacher("james", passwordEncoder.encode("password") , "example@gmail.com", temp);
-			teacher.setAuthorities(authorities);
+			// Create and save teacher
+			Teacher teacher = new Teacher("James", "example@gmail.com",
+					passwordEncoder.encode("password"), authorities);
+			teacher.setSubjects(subjects);
 			teacherRepository.save(teacher);
+
+			// Create and save student
+			Student student = new Student("Jhon Smith", "john@example.com",
+					passwordEncoder.encode("test"), 1, studentAuthorities);
+			student.setTeacher(teacher);
 			studentRepository.save(student);
+
+			// Set students to the teacher and update the teacher in the repository
+			teacher.setStudents(List.of(student));
+			teacherRepository.save(teacher);
+
+			// Create assignment list for the student
+			Assignments studentAssignments = new Assignments("math", 1, student);
+
+			// Save the assignments object
+			assignmentsRepository.save(studentAssignments);
+
+			// Create and save an assignment
+			Assignment assignment = new Assignment("Adding and subtracting", 100.0,
+					true, false, LocalDate.now(), AssignmentType.ASSIGNMENT.toString(), studentAssignments);
+			assignmentRepository.save(assignment);
+
+			studentAssignments.setAssignments(List.of(assignment));
+			student.setAssignments(List.of(studentAssignments));
+			assignmentsRepository.save(studentAssignments);
+			studentRepository.save(student);
+			teacherRepository.save(teacher);
+
 		};
 	}
+
 }
