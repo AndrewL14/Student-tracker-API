41838327f53cc3374c53272450719022faeb14a2
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-06-13 00:43:33 -0500

    Created a edit teacher request

--- src/main/java/com/tracer/model/request/teacher/EditTeacherRequest.java
+++ src/main/java/com/tracer/model/request/teacher/EditTeacherRequest.java
@@ -1,2 +1,20 @@
-package com.tracer.model.request.teacher;public record EditTeacherRequest() {
+package com.tracer.model.request.teacher;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class EditTeacherRequest {
+    private String username = "";
+    private String email = "";
+    private String password = "";
+    private Set<String> subjects = new HashSet<>();
 }
